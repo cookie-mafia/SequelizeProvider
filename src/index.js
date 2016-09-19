9eb7c62e7036e441aab92b2e44ae6b0a873bd552
nakautot <jpanuncillo@live.com>
2016-09-19 17:47:45 +0800

    feature: prepare shell for provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+function doFilter() {
+  return 'Filtering is not supported.';
+}
+
+function doSort() {
+  return 'Sorting is not supported.';
+}
+
+function doBatch() {
+  return 'Batch fetch is not supported.';
+}
+
+function execute() {
+  return 'Execution is not supported.';
+}
+
 module.exports = {
-  'version': '1.0.0'
+  'version': '1.0.0',
+  doFilter,
+  doSort,
+  doBatch,
+  execute
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,26 @@
 'use strict';
 
-require('should');
-
+const should  = require('should/as-function');
 const secProv = require('./index.js');
 
 describe('attributes', () => {
   it('version check', () => {
-    secProv.should.have.property('version');
+    should(secProv).have.property('version');
+  });
+
+  it('should have doFilter attribute', () => {
+    should(secProv.doFilter()).equal('Filtering is not supported.');
+  });
+
+  it('should have doSort attribute', () => {
+    should(secProv.doSort()).equal('Sorting is not supported.');
+  });
+
+  it('should have doBatch attribute', () => {
+    should(secProv.doBatch()).equal('Batch fetch is not supported.');
+  });
+
+  it('should have execute attribute', () => {
+    should(secProv.execute()).equal('Execution is not supported.');
   });
 });
